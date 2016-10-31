1f8277f212215982d08e61a450d00fe35efffe76
Alex Nesterov <ialexnesterov@gmail.com>
2016-11-01 02:47:12 +0300

    Add one page scroll with fullpage

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,3 +1,36 @@
+// One Page Scroll
+$(function(){
+
+  $('#fullpage').fullpage({
+    verticalCentered: false,
+    onLeave: function(index, nextIndex, direction) {
+      console.log(nextIndex);
+      $('.fixed-menu__item').eq(nextIndex - 1).addClass('is-active')
+        .siblings()
+        .removeClass('is-active');
+    }
+  });
+
+  $('.arrow-down').on('click', function(e) {
+    e.preventDefault();
+    $.fn.fullpage.moveSectionDown();
+  });
+
+  $('.fixed-menu__link, .nav__link').on('click', function(e) {
+    e.preventDefault();
+
+    var $this = $(this),
+        href = parseInt($this.attr('href'));
+
+    $this.closest('li').addClass('is-active')
+      .siblings()
+      .removeClass('is-active');
+
+    $.fn.fullpage.moveTo(href);
+  });
+
+});
+
 // Slider
 $(function(){
 
